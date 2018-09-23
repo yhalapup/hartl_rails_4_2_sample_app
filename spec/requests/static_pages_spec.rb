9042782de17ec7388ec5ab01d374f5d6599fa500
Yurii Halapup <halapup.yurii@gmail.com>
2018-09-23 15:04:44 +0300

    add test for non-existing page to show work with TDD

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,4 +14,11 @@ RSpec.describe "Static pages", type: :request do
       expect(page).to have_content('Help')
     end
   end
+
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+  end
 end
