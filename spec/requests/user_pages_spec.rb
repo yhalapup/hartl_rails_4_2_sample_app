8b10614d05c77b81e9afd93a130252c323776648
Yurii Halapup <halapup.yurii@gmail.com>
2018-09-27 23:08:31 +0300

    add tests for sign up page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "UserPages", type: :request do
-  describe "GET /user_pages" do
-    it "works! (now write some real specs)" do
-      get user_pages_index_path
-      expect(response).to have_http_status(200)
-    end
+
+  subject { page }
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { is_expected.to have_content('Sign up') }
+    it { is_expected.to have_title('Sign up') }
   end
 end
