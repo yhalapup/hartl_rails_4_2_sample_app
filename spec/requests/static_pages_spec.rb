a3dd563417ace899cb93a93582634bb7cbf33baf
Yurii Halapup <halapup.yurii@gmail.com>
2018-09-27 22:52:56 +0300

    make tests more dry with subject { page }

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,55 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe "Static pages", type: :request do
+
+  subject { page }
+
   describe "Home page" do
     before { visit root_path }
 
-    it "should have the content 'Sample App'" do
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have the base title" do
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
-    end
-
-    it "should not have a custom page title" do
-      expect(page).not_to have_title("| Home")
-    end
+    it { is_expected.to have_content('Sample App') }
+    it { is_expected.to have_title("Ruby on Rails Tutorial Sample App")}
+    it { is_expected.not_to have_title("| Home")}
   end
 
   describe "Help page" do
     before { visit help_path }
 
-    it "should have the content 'Help'" do
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the title 'Help'" do
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
-    end
+    it { is_expected.to have_content('Help')}
+    it { is_expected.to have_title("Ruby on Rails Tutorial Sample App | Help")}
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it "should have the content 'About Us'" do
-      expect(page).to have_content('About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
-    end
+    it { is_expected.to have_content('About Us') }
+    it { is_expected.to have_title("Ruby on Rails Tutorial Sample App | About Us")}
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
-    it "should have the content 'Contact'" do
-      expect(page).to have_content('Contact')
-    end
-
-    it "should have the title 'Contact'" do
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
-    end
+    it { is_expected.to have_content('Contact')}
+    it { is_expected.to have_title("Ruby on Rails Tutorial Sample App | Contact")}
   end
 end
