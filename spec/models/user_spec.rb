080ebc2f550217b953f8f897b6bc90cbdd50053d
Yurii Halapup <halapup.yurii@gmail.com>
2018-09-29 11:47:45 +0300

    test User model for existence of name and email attributes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = User.new(name: "Example User", email: "user@example.com") }
+
+  subject { @user }
+
+  it { is_expected.to respond_to(:name) }
+  it { is_expected.to respond_to(:email) }
 end
