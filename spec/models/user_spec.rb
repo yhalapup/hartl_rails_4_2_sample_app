fc61c3e4a70c6a05a2d8e6bfa25497147117d61f
Yurii Halapup <halapup.yurii@gmail.com>
2018-09-29 13:56:19 +0300

    add test user should have password_digest

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe User, type: :model do
 
   it { is_expected.to respond_to(:name) }
   it { is_expected.to respond_to(:email) }
+  it { is_expected.to respond_to(:password_digest) }
 
   it { is_expected.to be_valid }
 
