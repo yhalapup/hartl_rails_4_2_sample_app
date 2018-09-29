661ef21c626cfa94335268b22e7326a27717e3bf
Yurii Halapup <halapup.yurii@gmail.com>
2018-09-29 15:03:54 +0300

    add tests for user password authentication

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,7 @@ RSpec.describe User, type: :model do
   it { is_expected.to respond_to(:password_digest) }
   it { is_expected.to respond_to(:password) }
   it { is_expected.to respond_to(:password_confirmation) }
+  it { is_expected.to respond_to(:authenticate) }
 
   it { is_expected.to be_valid }
 
@@ -78,5 +79,23 @@ RSpec.describe User, type: :model do
         password: " ", password_confirmation: " ")
     end
     it { is_expected.not_to be_valid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by(email: @user.email) }
+
+    describe "with valid password" do
+      it { is_expected.to eq(found_user.authenticate(@user.password)) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { is_expected.not_to eq(user_for_invalid_password) }
+      # specify is just synonim for it
+      # it can be used when writing it sounds unnatural.
+      specify { expect(user_for_invalid_password).to be_falsey }
+    end
   end
 end
