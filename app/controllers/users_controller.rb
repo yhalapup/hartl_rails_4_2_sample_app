b6ae123b129f75afbdeaab0c045bd644a673c1be
Yurii Halapup <halapup.yurii@gmail.com>
2018-09-30 18:07:31 +0300

    show successful flash message after successful user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Sample App"
       # code below is the same as
       # redirect_to user_path(@user)
       redirect_to @user
