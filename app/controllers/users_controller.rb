005f206a237efdf50c1b42f17940ee4d15d1ff40
Yurii Halapup <halapup.yurii@gmail.com>
2018-09-30 17:59:47 +0300

    if successful signup redirect user to his profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # handle a successful save.
+      # code below is the same as
+      # redirect_to user_path(@user)
+      redirect_to @user
     else
       render 'new'
     end
