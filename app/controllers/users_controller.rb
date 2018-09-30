198701cbd18a2ccdd26bbc3dc69bfcb212e9544c
Yurii Halapup <halapup.yurii@gmail.com>
2018-09-30 17:23:18 +0300

    add strong parameters in users_controller defining private method user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       # handle a successful save.
     else
@@ -15,5 +15,13 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name, :email, :password, :password_confirmation
+    )
   end
 end
