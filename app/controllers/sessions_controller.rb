d5afdc3928fb6001be9dd133be92c21d0c065105
Yurii Halapup <halapup.yurii@gmail.com>
2018-10-12 12:44:36 +0300

    add basic authentication if create action of sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Sign the user in and redirect to the user's show page.
+    else
+      # Create an error message and rerender the signin form.
+      render 'new'
   end
 
   def destroy
