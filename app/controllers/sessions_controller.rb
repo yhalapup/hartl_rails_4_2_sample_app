52af96b5d557e189b3492e5ce86caa525db78088
Yurii Halapup <halapup.yurii@gmail.com>
2018-10-12 12:58:26 +0300

    show flash message if submit invalid data

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,9 @@ class SessionsController < ApplicationController
       # Sign the user in and redirect to the user's show page.
     else
       # Create an error message and rerender the signin form.
+      flash[:error] = 'Invalid email/password combination'
       render 'new'
+    end
   end
 
   def destroy
